8ae5020caca8b6565d8b17c02ca29a317bedfbea
Fernando Sproviero <fernandospr@gmail.com>
2013-06-17 15:20:26 -0300

    Added Tile and Toast templates

--- src/main/java/ar/com/fernandospr/wns/model/types/WnsTileTemplate.java
+++ src/main/java/ar/com/fernandospr/wns/model/types/WnsTileTemplate.java
@@ -2,7 +2,23 @@ package ar.com.fernandospr.wns.model.types;
 
 // http://msdn.microsoft.com/en-us/library/windows/apps/hh761491.aspx
 public final class WnsTileTemplate {
-	public final class SquareTextOnly {
-
+	public static final class SquareTextOnly {
+		public static final String TileSquareBlock = "TileSquareBlock";
+		public static final String TileSquareText01 = "TileSquareText01";
+		public static final String TileSquareText02 = "TileSquareText02";
+		public static final String TileSquareText03 = "TileSquareText03";
+		public static final String TileSquareText04 = "TileSquareText04";
+	}
+	public static final class SquareImageOnly {
+		public static final String TileSquarePeekImageAndText01 = "TileSquarePeekImageAndText01";
+		public static final String TileSquarePeekImageAndText02 = "TileSquarePeekImageAndText02";
+		public static final String TileSquarePeekImageAndText03 = "TileSquarePeekImageAndText03";
+		public static final String TileSquarePeekImageAndText04 = "TileSquarePeekImageAndText04";
+	}
+	public static final class SquarePeek {
+		public static final String TileSquarePeekImageAndText01 = "TileSquarePeekImageAndText01";
+		public static final String TileSquarePeekImageAndText02 = "TileSquarePeekImageAndText02";
+		public static final String TileSquarePeekImageAndText03 = "TileSquarePeekImageAndText03";
+		public static final String TileSquarePeekImageAndText04 = "TileSquarePeekImageAndText04";
 	}
 }
--- src/main/java/ar/com/fernandospr/wns/model/types/WnsToastTemplate.java
+++ src/main/java/ar/com/fernandospr/wns/model/types/WnsToastTemplate.java
@@ -2,8 +2,16 @@ package ar.com.fernandospr.wns.model.types;
 
 // http://msdn.microsoft.com/en-us/library/windows/apps/hh761494.aspx
 public final class WnsToastTemplate {
-	public final class TextOnly {
+	public static final class TextOnly {
 		public static final String ToastText01 = "ToastText01";
 		public static final String ToastText02 = "ToastText02";
+		public static final String ToastText03 = "ToastText03";
+		public static final String ToastText04 = "ToastText04";
+	}
+	public static final class ImageAndText {
+		public static final String ToastImageAndText01 = "ToastImageAndText01";
+		public static final String ToastImageAndText02 = "ToastImageAndText02";
+		public static final String ToastImageAndText03 = "ToastImageAndText03";
+		public static final String ToastImageAndText04 = "ToastImageAndText04";
 	}
 }

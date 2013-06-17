63bf2bdb012fda7bf06e8506babdac38894abd7a
Fernando Sproviero <fernandospr@gmail.com>
2013-06-17 10:05:27 -0300

    Initial maven project

---
+++ src/main/java/ar/com/fernandospr/wns/App.java
@@ -0,0 +1,13 @@
+package ar.com.fernandospr.wns;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

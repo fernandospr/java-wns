2772ee599006030e84fb52748d591c1df668569e
Fernando Sproviero <fernandospr@gmail.com>
2013-06-18 17:45:33 -0300

    Service updated

---
+++ src/main/java/ar/com/fernandospr/wns/model/WnsNotificationResponse.java
@@ -0,0 +1,25 @@
+package ar.com.fernandospr.wns.model;
+
+import javax.ws.rs.core.MultivaluedMap;
+
+/**
+ * From http://msdn.microsoft.com/en-us/library/windows/apps/hh465435.aspx#send_notification_response
+ */
+public class WnsNotificationResponse {
+	public final int code;
+	
+	public final String debugTrace;
+	public final String deviceConnectionStatus;
+	public final String errorDescription;
+	public final String msgID;
+	public final String notificationStatus;
+	
+	public WnsNotificationResponse(int responseCode, MultivaluedMap<String, String> headers) {
+		this.code = responseCode;
+		this.debugTrace = headers.get("X-WNS-Debug-Trace") != null ? headers.get("X-WNS-Debug-Trace").get(0) : null;
+		this.deviceConnectionStatus = headers.get("X-WNS-DeviceConnectionStatus") != null ? headers.get("X-WNS-DeviceConnectionStatus").get(0) : null;
+		this.errorDescription = headers.get("X-WNS-Error-Description") != null ? headers.get("X-WNS-Error-Description").get(0) : null;
+		this.msgID = headers.get("X-WNS-Msg-ID") != null ? headers.get("X-WNS-Msg-ID").get(0) : null;
+		this.notificationStatus = headers.get("X-WNS-NotificationStatus") != null ? headers.get("X-WNS-NotificationStatus").get(0) : null;
+	}
+}

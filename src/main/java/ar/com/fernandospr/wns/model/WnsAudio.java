5b8e16807b7290ed0b644fff533de1357ffcf7ab
Fernando Sproviero <fernandospr@gmail.com>
2013-06-18 12:52:39 -0300

    Model updated

--- src/main/java/ar/com/fernandospr/wns/model/WnsAudio.java
+++ src/main/java/ar/com/fernandospr/wns/model/WnsAudio.java
@@ -1,8 +1,11 @@
 package ar.com.fernandospr.wns.model;
 
 import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
 
 // http://msdn.microsoft.com/en-us/library/windows/apps/br230842.aspx
+
+@XmlRootElement(name = "audio")
 public class WnsAudio {
 	
 	@XmlAttribute
--- src/main/java/ar/com/fernandospr/wns/model/WnsToast.java
+++ src/main/java/ar/com/fernandospr/wns/model/WnsToast.java
@@ -2,8 +2,11 @@ package ar.com.fernandospr.wns.model;
 
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 
 // http://msdn.microsoft.com/en-us/library/windows/apps/br230846.aspx
+
+@XmlRootElement(name = "toast")
 public class WnsToast {
 	
 	@XmlAttribute

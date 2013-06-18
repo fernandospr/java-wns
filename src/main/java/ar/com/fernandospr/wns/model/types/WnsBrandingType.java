36485a86ce899ee14ec440bbf08c6b6fc4c51b4d
Fernando Sproviero <fernandospr@gmail.com>
2013-06-18 11:01:02 -0300

    Added branding types

---
+++ src/main/java/ar/com/fernandospr/wns/model/types/WnsBrandingType.java
@@ -0,0 +1,7 @@
+package ar.com.fernandospr.wns.model.types;
+
+public final class WnsBrandingType {
+	public static final String NONE = "none";
+	public static final String LOGO = "logo";
+	public static final String NAME = "name";
+}

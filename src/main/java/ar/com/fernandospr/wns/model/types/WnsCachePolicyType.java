68e66cf16f5da91573afe7949e34f30477d901f5
Fernando Sproviero <fernandospr@gmail.com>
2013-08-08 11:00:47 -0300

    Fixing cache policy type

--- src/main/java/ar/com/fernandospr/wns/model/types/WnsCachePolicyType.java
+++ src/main/java/ar/com/fernandospr/wns/model/types/WnsCachePolicyType.java
@@ -5,5 +5,5 @@ package ar.com.fernandospr.wns.model.types;
  */
 public final class WnsCachePolicyType {
 	public static final String CACHE = "cache";
-	public static final String NOCACHE = "nocache";
+	public static final String NOCACHE = "no-cache";
 }

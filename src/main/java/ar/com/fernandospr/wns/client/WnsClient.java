67445d9b473d8def438cc61c709df7b441517adc
chrisdoc <christoph@meedoc.com>
2015-03-06 13:58:07 +0200

    add gitignore update jersey to version 2 updating WNSClient to use jersey 2 and jackson 2

--- src/main/java/ar/com/fernandospr/wns/client/WnsClient.java
+++ src/main/java/ar/com/fernandospr/wns/client/WnsClient.java
@@ -2,31 +2,25 @@ package ar.com.fernandospr.wns.client;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
+import javax.ws.rs.client.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
 
-import org.apache.commons.httpclient.auth.AuthScope;
-import org.codehaus.jackson.jaxrs.JacksonJsonProvider;
 
 import ar.com.fernandospr.wns.WnsProxyProperties;
 import ar.com.fernandospr.wns.exceptions.WnsException;
-import ar.com.fernandospr.wns.model.WnsAbstractNotification;
-import ar.com.fernandospr.wns.model.WnsNotificationRequestOptional;
-import ar.com.fernandospr.wns.model.WnsNotificationResponse;
-import ar.com.fernandospr.wns.model.WnsOAuthToken;
-
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.WebResource;
-import com.sun.jersey.api.client.WebResource.Builder;
-import com.sun.jersey.api.client.config.ClientConfig;
-import com.sun.jersey.api.client.config.DefaultClientConfig;
-import com.sun.jersey.client.apache.ApacheHttpClient;
-import com.sun.jersey.client.apache.config.DefaultApacheHttpClientConfig;
-import com.sun.jersey.api.client.filter.LoggingFilter;
-import com.sun.jersey.client.apache.config.ApacheHttpClientConfig;
-import com.sun.jersey.core.util.MultivaluedMapImpl;
+import ar.com.fernandospr.wns.model.*;
+import ar.com.fernandospr.wns.model.types.WnsNotificationType;
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import com.fasterxml.jackson.jaxrs.xml.JacksonJaxbXMLProvider;
+import org.glassfish.jersey.apache.connector.ApacheConnectorProvider;
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.client.ClientProperties;
+import org.glassfish.jersey.filter.LoggingFilter;
+import org.glassfish.jersey.internal.util.collection.MultivaluedStringMap;
 
 
 public class WnsClient {
@@ -51,72 +45,76 @@ public class WnsClient {
 		this.clientSecret = clientSecret;
 		this.client = createClient(logging, proxyProps);
 	}
-	
-	private static Client createClient(boolean logging) {
-		ClientConfig clientConfig = new DefaultClientConfig();
-		clientConfig.getClasses().add(JacksonJsonProvider.class);
-		Client client = Client.create(clientConfig);
-		if (logging == true) {
-			client.addFilter(new LoggingFilter(System.out));
-		}
-		return client;
-	}
-	
-	private static Client createClient(boolean logging, WnsProxyProperties proxyProps) {
-		DefaultApacheHttpClientConfig clientConfig = new DefaultApacheHttpClientConfig();
-		setProxyCredentials(clientConfig, proxyProps);
-		clientConfig.getClasses().add(JacksonJsonProvider.class);
-		Client client = ApacheHttpClient.create(clientConfig);
-		if (logging == true) {
-			client.addFilter(new LoggingFilter(System.out));
-		}
-		return client;
-	}
-	
-	private static void setProxyCredentials(DefaultApacheHttpClientConfig clientConfig, WnsProxyProperties proxyProps) {
-		if(proxyProps != null) {
-			String  proxyProtocol   = proxyProps.getProtocol();
-			String  proxyHost  	= proxyProps.getHost();
-			int     proxyPort  	= proxyProps.getPort();
-			String  proxyUser	= proxyProps.getUser();
-			String  proxyPass  	= proxyProps.getPass();
-			
-			if ((proxyHost != null) && (!proxyHost.trim().isEmpty())) {
-				
-				clientConfig.getProperties().put(ApacheHttpClientConfig.PROPERTY_PROXY_URI, proxyProtocol + "://" + proxyHost + ":" + proxyPort);
-				
-				if (!proxyUser.trim().isEmpty()) {
-					clientConfig.getProperties().put(ApacheHttpClientConfig.PROPERTY_PREEMPTIVE_AUTHENTICATION, true);
-					clientConfig.getProperties().put(ApacheHttpClientConfig.PROPERTY_INTERACTIVE, true);
-					clientConfig.getState().setCredentials(AuthScope.ANY_REALM, AuthScope.ANY_HOST, AuthScope.ANY_PORT, proxyUser, proxyPass);
-					clientConfig.getState().setProxyCredentials(AuthScope.ANY_REALM, proxyHost, proxyPort, proxyUser, proxyPass);
-				}
-			}
-			
-		}
-		
-	}
-	
-	/**
-	 * Based on <a href="http://msdn.microsoft.com/en-us/library/windows/apps/hh465407.aspx">http://msdn.microsoft.com/en-us/library/windows/apps/hh465407.aspx</a>
-	 * @throws WnsException when authentication fails
-	 */
-	public void refreshAccessToken() throws WnsException {
-		WebResource webResource = this.client.resource(AUTHENTICATION_URI);
-		MultivaluedMap<String, String> formData = new MultivaluedMapImpl();
-		formData.add("grant_type", GRANT_TYPE_CLIENT_CREDENTIALS);
-		formData.add("client_id", this.sid);
-		formData.add("client_secret", this.clientSecret);
-		formData.add("scope", SCOPE);
-		ClientResponse response = webResource.type(MediaType.APPLICATION_FORM_URLENCODED_TYPE)
-											 .accept(MediaType.APPLICATION_JSON_TYPE)
-											 .post(ClientResponse.class, formData);
-		if (response.getStatus() != 200) {
-			throw new WnsException("Authentication failed. HTTP error code: " + response.getStatus());
-		}
-		
-		this.token = response.getEntity(WnsOAuthToken.class);
-	}
+
+    private static Client createClient(boolean logging) {
+        ClientConfig clientConfig = new ClientConfig(JacksonJaxbXMLProvider.class, JacksonJsonProvider.class);
+        Client client = ClientBuilder.newClient(clientConfig);
+
+        if (logging) {
+            LoggingFilter loggingFilter = new LoggingFilter(
+                    Logger.getLogger(WnsClient.class.getName()), true);
+
+            client = client.register(loggingFilter);
+        }
+        return client;
+    }
+
+    private static Client createClient(boolean logging, WnsProxyProperties proxyProps) {
+        ClientConfig clientConfig = new ClientConfig(JacksonJaxbXMLProvider.class, JacksonJsonProvider.class)
+                .connectorProvider(new ApacheConnectorProvider());
+        setProxyCredentials(clientConfig, proxyProps);
+
+        Client client = ClientBuilder.newClient(clientConfig);
+        if (logging) {
+            LoggingFilter loggingFilter = new LoggingFilter(
+                    Logger.getLogger(WnsClient.class.getName()), true);
+
+            client = client.register(loggingFilter);
+        }
+        return client;
+    }
+
+    private static void setProxyCredentials(ClientConfig clientConfig, WnsProxyProperties proxyProps) {
+        if (proxyProps != null) {
+            String proxyProtocol = proxyProps.getProtocol();
+            String proxyHost = proxyProps.getHost();
+            int proxyPort = proxyProps.getPort();
+            String proxyUser = proxyProps.getUser();
+            String proxyPass = proxyProps.getPass();
+
+            if ((proxyHost != null) && (!proxyHost.trim().isEmpty())) {
+                clientConfig.property(ClientProperties.PROXY_URI, proxyProtocol + "://" + proxyHost + ":" + proxyPort);
+                if (!proxyUser.trim().isEmpty()) {
+                    clientConfig.property(ClientProperties.PROXY_PASSWORD, proxyPass);
+                    clientConfig.property(ClientProperties.PROXY_USERNAME, proxyUser);
+                }
+            }
+
+        }
+
+    }
+
+    /**
+     * Based on <a href="http://msdn.microsoft.com/en-us/library/windows/apps/hh465407.aspx">http://msdn.microsoft.com/en-us/library/windows/apps/hh465407.aspx</a>
+     *
+     * @throws WnsException when authentication fails
+     */
+    public void refreshAccessToken() throws WnsException {
+        WebTarget target = client.target(AUTHENTICATION_URI);
+
+        MultivaluedStringMap formData = new MultivaluedStringMap();
+        formData.add("grant_type", GRANT_TYPE_CLIENT_CREDENTIALS);
+        formData.add("client_id", this.sid);
+        formData.add("client_secret", this.clientSecret);
+        formData.add("scope", SCOPE);
+        Response response = target.request(MediaType.APPLICATION_FORM_URLENCODED_TYPE).accept(MediaType.APPLICATION_JSON_TYPE).post(Entity.form(formData));
+
+        if (response.getStatus() != 200) {
+            throw new WnsException("Authentication failed. HTTP error code: " + response.getStatus());
+        }
+
+        this.token = response.readEntity(WnsOAuthToken.class);
+    }
 	
 	/**
 	 * @param channelUri
@@ -127,13 +125,13 @@ public class WnsClient {
 	 * @throws WnsException when authentication fails
 	 */
 	public WnsNotificationResponse push(WnsResourceBuilder resourceBuilder, String channelUri, WnsAbstractNotification notification, int retriesLeft, WnsNotificationRequestOptional optional) throws WnsException {
-		WebResource webResource = this.client.resource(channelUri);
-		
-		Builder webResourceBuilder = resourceBuilder.build(webResource, notification, getToken().access_token, optional);
-		
-		ClientResponse response = webResourceBuilder.post(ClientResponse.class);
-		
-		WnsNotificationResponse notificationResponse = new WnsNotificationResponse(channelUri, response.getStatus(), response.getHeaders());
+        WebTarget target = client.target(channelUri);
+        Invocation.Builder webResourceBuilder = resourceBuilder.build(target, notification, getToken().access_token, optional);
+        String type = notification.getType().equals(WnsNotificationType.RAW) ? MediaType.APPLICATION_OCTET_STREAM : MediaType.TEXT_XML;
+
+        Response response = webResourceBuilder.buildPost(Entity.entity(resourceBuilder.getEntityToSendWithNotification(notification), type)).invoke();
+
+        WnsNotificationResponse notificationResponse = new WnsNotificationResponse(channelUri, response.getStatus(), response.getStringHeaders()    );
 		if (notificationResponse.code == 200) {
 			return notificationResponse;
 		}
--- src/main/java/ar/com/fernandospr/wns/client/WnsRawResourceBuilder.java
+++ src/main/java/ar/com/fernandospr/wns/client/WnsRawResourceBuilder.java
@@ -1,18 +1,18 @@
 package ar.com.fernandospr.wns.client;
 
+import javax.ws.rs.client.Invocation;
 import javax.ws.rs.core.MediaType;
 
 import ar.com.fernandospr.wns.model.WnsAbstractNotification;
 import ar.com.fernandospr.wns.model.WnsRaw;
 
-import com.sun.jersey.api.client.WebResource.Builder;
 
 public class WnsRawResourceBuilder extends WnsResourceBuilder {	
 	
 	@Override
-	protected void addRequiredHeaders(Builder webResourceBuilder, String type, String accessToken) {
+	protected void addRequiredHeaders(Invocation.Builder webResourceBuilder, String type, String accessToken) {
 		super.addRequiredHeaders(webResourceBuilder, type, accessToken);
-		webResourceBuilder.type(MediaType.APPLICATION_OCTET_STREAM);
+		webResourceBuilder.accept(MediaType.APPLICATION_OCTET_STREAM);
 	}
 
 	@Override

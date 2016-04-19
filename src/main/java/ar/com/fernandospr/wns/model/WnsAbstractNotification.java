6441a5a0ddd4297b2746881e52945b5f0a1afd05
Sergio-Mira <sergio.mira@swrve.com>
2016-04-19 13:08:38 +0100

    Remove type from XML and do not serialize empty lists

--- src/main/java/ar/com/fernandospr/wns/model/WnsAbstractNotification.java
+++ src/main/java/ar/com/fernandospr/wns/model/WnsAbstractNotification.java
@@ -1,5 +1,8 @@
 package ar.com.fernandospr.wns.model;
 
+import javax.xml.bind.annotation.XmlTransient;
+
 public abstract class WnsAbstractNotification {
+	@XmlTransient
 	public abstract String getType();
 }
--- src/main/java/ar/com/fernandospr/wns/model/WnsBinding.java
+++ src/main/java/ar/com/fernandospr/wns/model/WnsBinding.java
@@ -1,5 +1,6 @@
 package ar.com.fernandospr.wns.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 
 import java.util.List;
@@ -11,24 +12,25 @@ import javax.xml.bind.annotation.XmlRootElement;
 /**
  * From <a href="http://msdn.microsoft.com/en-us/library/windows/apps/br230843.aspx">http://msdn.microsoft.com/en-us/library/windows/apps/br230843.aspx</a>
  */
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 @XmlRootElement(name = "binding")
 public class WnsBinding {
-	
+
 	@XmlAttribute
 	public String template;
-	
+
 	@XmlAttribute
 	public String fallback;
-	
+
 	@XmlAttribute
 	public String lang;
-	
+
 	@XmlAttribute
 	public String baseUri;
-	
+
 	@XmlAttribute
 	public String branding;
-	
+
 	@XmlAttribute
 	public Boolean addImageQuery;
 

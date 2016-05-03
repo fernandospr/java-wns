a5aa45b58f397b82a3c8677948ff167ed3dea21b
Fernando Sproviero <fernandospr@gmail.com>
2016-05-03 20:12:15 -0300

    Fix in badge types.

--- src/main/java/ar/com/fernandospr/wns/model/types/WnsBadgeType.java
+++ src/main/java/ar/com/fernandospr/wns/model/types/WnsBadgeType.java
@@ -8,7 +8,8 @@ public final class WnsBadgeType {
 	public static final String ACTIVITY = "activity";
 	public static final String ALERT = "alert";
 	public static final String AVAILABLE = "available";
-	public static final String AWAY = "available";
+	public static final String AWAY = "away";
+	public static final String ALARM = "alarm";
 	public static final String BUSY = "busy";
 	public static final String NEWMESSAGE = "newMessage";
 	public static final String PAUSED = "paused";
